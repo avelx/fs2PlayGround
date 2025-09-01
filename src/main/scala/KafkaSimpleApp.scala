e6190d2be9f99378f99e886488ea3c668d063a7a
Pavel <pavel.oliynyk@gmail.com>
2025-09-01 15:45:23 +0100

    feat: create simple consumer

--- src/main/scala/KafkaSimpleApp.scala
+++ src/main/scala/KafkaSimpleApp.scala
@@ -1,4 +1,6 @@
 import cats.effect.{IO, IOApp}
+import fs2.Chunk
+import fs2.kafka.consumer.KafkaConsumeChunk.CommitNow
 import fs2.kafka.{AutoOffsetReset, ConsumerRecord, ConsumerSettings, KafkaConsumer, KafkaProducer, ProducerRecord, ProducerRecords, ProducerSettings}
 
 import scala.concurrent.duration.DurationInt
@@ -8,26 +10,27 @@ object KafkaSimpleApp extends IOApp.Simple {
   val run: IO[Unit] = {
     val producerSettings = ProducerSettings[IO, String, String].withBootstrapServers("localhost:9092")
 
-    val consumerSettings =  ConsumerSettings[IO, String, String]
-        .withAutoOffsetReset(AutoOffsetReset.Earliest)
-        .withBootstrapServers("localhost:9092")
-        .withGroupId("group")
+    val consumerSettings = ConsumerSettings[IO, String, String]
+      .withAutoOffsetReset(AutoOffsetReset.Earliest)
+      .withBootstrapServers("localhost:9092")
+      .withGroupId("group")
 
     val producerStream = KafkaProducer
       .stream(producerSettings)
       .flatMap { producer =>
-        fs2.Stream.emits(1 to 10).covary[IO].map { i =>
+        fs2.Stream.emits(1 to 100).covary[IO].map { i =>
             ProducerRecords.one(
               ProducerRecord("topic", s"key-$i", s"value-$i")
             )
           }
           .evalMap(producer.produce)
-          .groupWithin(500, 3.seconds)
+          .groupWithin(100, 3.seconds)
           .evalMap(_.sequence)
       }
 
     def processRecord(record: ConsumerRecord[String, String]): IO[Unit] =
-      IO(println(s"Processing record: $record"))
+      IO(println(s"Processing record: $record")).as(CommitNow)
+
 
     val consumerStream =
       KafkaConsumer
@@ -43,7 +46,8 @@ object KafkaSimpleApp extends IOApp.Simple {
 
 
     for {
-      _ <- consumerStream.compile.drain // producerStream.compile.drain
+      //_ <- producerStream.covary[IO].compile.drain
+      _ <- consumerStream.covary[IO].compile.drain
     } yield ()
 
   }
