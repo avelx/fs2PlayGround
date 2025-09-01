47582ee3ec9dcba1fe2554216b0d5a6178654cc7
Pavel <pavel.oliynyk@gmail.com>
2025-09-01 15:13:47 +0100

    feat: create simple consumer

--- src/main/scala/KafkaSimpleApp.scala
+++ src/main/scala/KafkaSimpleApp.scala
@@ -1,30 +1,49 @@
 import cats.effect.{IO, IOApp}
-import fs2.kafka.{AutoOffsetReset, ConsumerSettings, KafkaConsumer, KafkaProducer, ProducerRecord, ProducerRecords, ProducerSettings}
+import fs2.kafka.{AutoOffsetReset, ConsumerRecord, ConsumerSettings, KafkaConsumer, KafkaProducer, ProducerRecord, ProducerRecords, ProducerSettings}
 
 import scala.concurrent.duration.DurationInt
 
 object KafkaSimpleApp extends IOApp.Simple {
 
   val run: IO[Unit] = {
-    val producerSettings =ProducerSettings[IO, String, String].withBootstrapServers("localhost:9092")
+    val producerSettings = ProducerSettings[IO, String, String].withBootstrapServers("localhost:9092")
 
-    val stream =
-      KafkaProducer
-        .stream(producerSettings)
-        .flatMap { producer =>
-          fs2.Stream.emits(1 to 10).covary[IO].map { i =>
-              ProducerRecords.one(
-                            ProducerRecord("topic", s"key-$i", s"value-$i")
-                          )
-                        }
-            .evalMap(producer.produce)
-            .groupWithin(500, 15.seconds)
-            .evalMap(_.sequence)
+    val consumerSettings =  ConsumerSettings[IO, String, String]
+        .withAutoOffsetReset(AutoOffsetReset.Earliest)
+        .withBootstrapServers("localhost:9092")
+        .withGroupId("group")
+
+    val producerStream = KafkaProducer
+      .stream(producerSettings)
+      .flatMap { producer =>
+        fs2.Stream.emits(1 to 10).covary[IO].map { i =>
+            ProducerRecords.one(
+              ProducerRecord("topic", s"key-$i", s"value-$i")
+            )
+          }
+          .evalMap(producer.produce)
+          .groupWithin(500, 3.seconds)
+          .evalMap(_.sequence)
+      }
+
+    def processRecord(record: ConsumerRecord[String, String]): IO[Unit] =
+      IO(println(s"Processing record: $record"))
+
+    val consumerStream =
+      KafkaConsumer
+        .stream(consumerSettings)
+        .subscribeTo("topic")
+        .partitionedRecords
+        .map { partitionStream =>
+          partitionStream.evalMap { committable =>
+            processRecord(committable.record)
+          }
         }
+        .parJoinUnbounded
 
 
     for {
-      _ <- stream.compile.drain
+      _ <- consumerStream.compile.drain // producerStream.compile.drain
     } yield ()
 
   }
