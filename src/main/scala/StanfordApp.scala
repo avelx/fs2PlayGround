9d6c2f589e91d246e6f06d2b760f7f1ac3970576
Pavel <pavel.oliynyk@gmail.com>
2025-09-04 10:36:11 +0100

    feat: create StanfordApp for quick experimenting [2]

--- src/main/scala/StanfordApp.scala
+++ src/main/scala/StanfordApp.scala
@@ -2,9 +2,10 @@ import cats.effect.{IO, IOApp}
 import config.WikiConfig
 import org.typelevel.log4cats.SelfAwareStructuredLogger
 import org.typelevel.log4cats.slf4j.Slf4jLogger
+import processor.TextProcessor._
 
 object StanfordApp extends IOApp.Simple {
-  
+
   implicit val logger: SelfAwareStructuredLogger[IO] = Slf4jLogger.getLogger[IO]
 
   val appConfig = WikiConfig.load
@@ -12,15 +13,15 @@ object StanfordApp extends IOApp.Simple {
   private val dataSourcePath: os.Path = {
     os.pwd / "data" / "wiki-samples" / "165709.txt"
   }
-  
+
   def readFile(path: os.Path): String = {
     os.read.lines(path).mkString("")
   }
-  
+
   override def run: IO[Unit] = {
     for {
       text <- IO.blocking(readFile(dataSourcePath))
-      _ <- IO.println(text)
+      _ <- IO.blocking(exec(text))
     } yield ()
   }
 }
---
+++ src/main/scala/processor/TextProcessor.scala
@@ -0,0 +1,49 @@
+package processor
+
+import edu.stanford.nlp.pipeline.StanfordCoreNLP
+
+import java.util.Properties
+import scala.util.Try
+
+object TextProcessor {
+
+  def exec(text: String): Unit = {
+    // set up pipeline properties
+    val props = new Properties()
+    // set the list of annotators to run
+    props.setProperty("annotators", "tokenize,pos,lemma,ner,parse,depparse,coref,kbp,quote")
+    //props.setProperty("annotators", "tokenize,pos,lemma,ner,depparse")
+    props.setProperty("coref.algorithm", "neural")
+    // build pipeline
+    val pipeline = new StanfordCoreNLP(props)
+
+    // create a document object
+    val coreDoc = pipeline.processToCoreDocument(text)
+    //pipeline.annotate(coreDoc)
+
+    println("Annotations")
+    println(coreDoc.annotation())
+
+    println("Sentence")
+    coreDoc.sentences().forEach(s => {
+      println(s"S: ${s.text()}")
+      println(s"L: ${s.lemmas()}")
+      println(s"T: ${s.tokens()}")
+      println(s"Tags: ${s.nerTags()}")
+      s.entityMentions().forEach{ c =>
+        val cEm = Try {
+          c.canonicalEntityMention().get().toString
+        }.toOption
+        println(s"EM: $cEm")
+      }
+    })
+
+    println("Quotes")
+    coreDoc.quotes().forEach(q => {
+      println(s"SPK: ${q.speaker()}")
+    })
+
+    println(s"CRC: ${coreDoc.corefChains()}")
+
+  }
+}
